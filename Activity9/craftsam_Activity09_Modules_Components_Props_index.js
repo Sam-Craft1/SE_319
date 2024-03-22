d8c0f1a3feb1ec130c9211dd04f21c3a7cbcf341
Sam-Craft1 <sam@thecrafts.net>
2024-03-22 10:31:59 -0500

    Activity 9 push 3

--- Activity9/craftsam_Activity09_Modules_Components_Props_index.js
+++ Activity9/craftsam_Activity09_Modules_Components_Props_index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { UserCard } from "./UserCards.js";
+import { UserCard } from "./craftsam_Activity09_Modules_Components_Props_UserCard.js";
 
 /*
 Author: Sam Craft
